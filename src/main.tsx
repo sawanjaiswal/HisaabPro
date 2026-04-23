ba6f57ce4c97f8f3a2df260b0186d8edf90ee810
Sawan Jaiswal <sawanj2311@gmail.com>
2026-04-23 10:55:32 +0530

    ui: hide TanStack Query devtools floating button
    The dev-mode floating mascot was top-left, overlapping headers and
    distracting from real UI work. Devtools weren't being used; remove
    the import + button. Easy to add back if query debugging is needed.

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async'
 import { QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { queryClient } from './lib/query-client'
 import { AuthProvider } from './context/AuthContext'
 import { ThemeProvider } from './context/ThemeContext'
@@ -34,9 +33,6 @@ createRoot(document.getElementById('root')!).render(
             </LanguageProvider>
           </ThemeProvider>
         </BrowserRouter>
-        {import.meta.env.DEV && (
-          <ReactQueryDevtools initialIsOpen={false} buttonPosition="top-left" />
-        )}
       </QueryClientProvider>
     </HelmetProvider>
   </StrictMode>
